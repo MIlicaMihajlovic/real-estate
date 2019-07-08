6c11e1dd02ded2c89a7ca834e5b50d89667312ed
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2019-07-09 00:01:02 +0200

    Commit

---
+++ wp-config.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'milica' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'milicA!09' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'QkOpYH/LZ0uVOG[MgYL,+Xgj$:f(?C9/_T ,v6xUa VFi)55kNHodSEN(-{nj^8,' );
+define( 'SECURE_AUTH_KEY',  '{,w{JyX7(e| :vd(KSeTnKV$eC;dbuQ}F4w~SVlI9<[s0w3Oz@VAPROuMnxVC71b' );
+define( 'LOGGED_IN_KEY',    'EAd{VU.Z7J8K;EP{#DF5p?IfbJWmdm^|7p7jqnDz$T214ez/yHQ0^_yJkn,PYmp#' );
+define( 'NONCE_KEY',        'rrPOCJ pTbxYsw ud82BUw5Xh|1)!_kb5O<qVO%,)tRbKhb_$~ZTZfjV<~{m_9CH' );
+define( 'AUTH_SALT',        '(1!O/L8xNfet^/&NC{SqiS_:P%Fw^IWe?C?8G1QX;yYaI8dB{e7C*KHH{@=U!#K4' );
+define( 'SECURE_AUTH_SALT', '0<{t;IdNK:=!3bU>jB. %J?v#YeuMC0j9][oy1vWAh!3G[fMNNCyR:EM_r Qg89{' );
+define( 'LOGGED_IN_SALT',   'P&Z!MivbjkG~#eR1;tQh~L~k)I$dm^B4yR<r#q*Do-_];!FGxS(;2t1foDsz,ARt' );
+define( 'NONCE_SALT',       'UDO.}Vs@nvU|b]g~AQmsc[f^#H7j?Jq;h[]vf.orXQ$(lT[oTSnLFz3IY87}BE2C' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
+
+define('FS_METHOD','direct');
---
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -0,0 +1,181 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'chld_thm_cfg_parent','twentyseventeen-style','twentyseventeen-block-style' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+
+//Register Real Estate post type
+
+//call the function
+add_action( 'init', 'create_real_estate' );
+
+function create_real_estate() {
+	$labels = array(
+		'name'               => _x( 'Real Estate', 'post type general name', 'textdomain' ),
+		'singular_name'      => _x( 'Real State', 'post type singular name', 'textdomain' ),
+		'menu_name'          => _x( 'Real Estate', 'admin menu', 'textdomain' ),
+		'name_admin_bar'     => _x( 'Real Estate', 'add new on admin bar', 'textdomain' ),
+		'add_new'            => _x( 'Add New', 'real_estate', 'textdomain' ),
+		'add_new_item'       => __( 'Add New Real Estate', 'textdomain' ),
+		'new_item'           => __( 'New Real Estate', 'textdomain' ),
+		'edit_item'          => __( 'Edit Real Estate', 'textdomain' ),
+		'view_item'          => __( 'View Real Estate', 'textdomain' ),
+		'all_items'          => __( 'All Real Estate', 'textdomain' ),
+		'search_items'       => __( 'Search Real Estate', 'textdomain' ),
+		'parent_item_colon'  => __( 'Parent Real Estate:', 'textdomain' ),
+		'not_found'          => __( 'No real estate found.', 'textdomain' ),
+		'not_found_in_trash' => __( 'No real estate found in Trash.', 'textdomain' )
+	);
+
+	$args = array(
+        'label'              => __( 'real_estate' ),
+		'labels'             => $labels,
+        'description'        => __( 'Description.', 'textdomain' ),
+        'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'estate','with_front' => false ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+        'supports'           => array( 'title', 'image', 'author', 'thumbnail', 'revisions', 'custom-fields' ),
+        'taxonomies'         => array( 'location', 'type' ),
+	);
+
+	register_post_type( 'real_estate', $args );
+}
+
+// hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', 'create_real_estate_taxonomies', 0 );
+
+//Register taxonomies Location and Type
+function create_real_estate_taxonomies() {
+     
+        // Add new taxonomy Location
+        $labels = array(
+            'name'                       => _x( 'Locations', 'taxonomy general name', 'textdomain' ),
+            'singular_name'              => _x( 'Location', 'taxonomy singular name', 'textdomain' ),
+            'search_items'               => __( 'Search Locations', 'textdomain' ),
+            'popular_items'              => __( 'Popular Locations', 'textdomain' ),
+            'all_items'                  => __( 'All Locations', 'textdomain' ),
+            'parent_item'                => __( 'Parent Type' ),
+            'parent_item_colon'          => __( 'Parent Type:' ),
+            'edit_item'                  => __( 'Edit Location', 'textdomain' ),
+            'update_item'                => __( 'Update Location', 'textdomain' ),
+            'add_new_item'               => __( 'Add New Location', 'textdomain' ),
+            'new_item_name'              => __( 'New Location Name', 'textdomain' ),
+            'separate_items_with_commas' => __( 'Separate locations with commas', 'textdomain' ),
+            'add_or_remove_items'        => __( 'Add or remove locations', 'textdomain' ),
+            'choose_from_most_used'      => __( 'Choose from the most used locations', 'textdomain' ),
+            'not_found'                  => __( 'No locations found.', 'textdomain' ),
+            'menu_name'                  => __( 'Locations', 'textdomain' ),
+        );
+    
+        $args = array(
+            'hierarchical'          => false,
+            'labels'                => $labels,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'update_count_callback' => '_update_post_term_count',
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'location/%location%','with_front' => false ),
+        );
+
+        register_taxonomy( 'location', 'real_estate', $args );
+    
+        
+    
+        // Add new taxonomy Type
+        $labels = array(
+            'name'                       => _x( 'Types', 'taxonomy general name', 'textdomain' ),
+            'singular_name'              => _x( 'Type', 'taxonomy singular name', 'textdomain' ),
+            'search_items'               => __( 'Search Types', 'textdomain' ),
+            'popular_items'              => __( 'Popular Types', 'textdomain' ),
+            'all_items'                  => __( 'All Types', 'textdomain' ),
+            'parent_item'                => null,
+            'parent_item_colon'          => null,
+            'edit_item'                  => __( 'Edit Type', 'textdomain' ),
+            'update_item'                => __( 'Update Type', 'textdomain' ),
+            'add_new_item'               => __( 'Add New Type', 'textdomain' ),
+            'new_item_name'              => __( 'New Type Name', 'textdomain' ),
+            'separate_items_with_commas' => __( 'Separate types with commas', 'textdomain' ),
+            'add_or_remove_items'        => __( 'Add or remove types', 'textdomain' ),
+            'choose_from_most_used'      => __( 'Choose from the most used types', 'textdomain' ),
+            'not_found'                  => __( 'No types found.', 'textdomain' ),
+            'menu_name'                  => __( 'Types', 'textdomain' ),
+        );
+    
+        $args = array(
+            'hierarchical'          => false,
+            'labels'                => $labels,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'update_count_callback' => '_update_post_term_count',
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => 'type/%type%','with_front' => false ),
+        );
+    
+        register_taxonomy( 'type', 'real_estate', $args );
+    }
+
+    if( function_exists('acf_add_local_field_group') ):
+
+        acf_add_local_field_group(array (
+            'key' => 'group_1',
+            'title' => 'My Group',
+            'fields' => array (
+                array (
+                    'key' => 'field_1',
+                    'label' => 'Sub Title',
+                    'name' => 'sub_title',
+                    'type' => 'text',
+                ),
+                array (
+                    'key' => 'field_2',
+                    'label' => 'Image',
+                    'name' => 'image',
+                    'type' => 'image',
+                )
+            ),
+            'location' => array (
+                array (
+                    array (
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'real_estate',
+                    ),
+                ),
+            ),
+        ));
+        
+        endif;
